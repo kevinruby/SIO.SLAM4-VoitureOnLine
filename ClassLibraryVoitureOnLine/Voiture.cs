63fd96f5abfad1820de09643dab00626b84a91be
RK <ruby.kevin57@gmail.com>
2014-12-05 18:16:39 +0100

    Début du projet

---
+++ ClassLibraryVoitureOnLine/Voiture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryVoitureOnLine
+{
+    public class Voiture
+    {
+        /// <summary>
+        /// La couleur.
+        /// </summary>
+        private String couleur;
+
+        /// <summary>
+        /// La finition extérieur.
+        /// </summary>
+        private String finitionExt;
+
+        /// <summary>
+        /// La finition intérieur.
+        /// </summary>
+        private String finitionInt;
+
+        /// <summary>
+        /// La motorisation.
+        /// </summary>
+        private String motorisation;
+
+        /// <summary>
+        /// Constructeur de classe.
+        /// </summary>
+        /// <param name="finitionInt">La finition intérieur</param>
+        /// <param name="finitionExt">La finition extérieur</param>
+        /// <param name="couleur">La couleur</param>
+        /// <param name="motorisation">La motorisation</param>
+        public Voiture(String finitionInt, String finitionExt, String couleur, String motorisation)
+        {
+            this.couleur = couleur;
+            this.finitionExt = finitionExt;
+            this.finitionInt = finitionInt;
+            this.motorisation = motorisation;
+        }
+
+        /// <summary>
+        /// Méthode qui calcule le prix.
+        /// </summary>
+        /// <returns>Le prix</returns>
+        public virtual double Prix()
+        {
+            switch (motorisation)
+            {
+                case 
+            }
+            return 10000;
+        }
+
+        /// <summary>
+        /// Méthode qui retourne une chaine qui représente la classe.
+        /// </summary>
+        /// <returns>Une chaine représentant la classe</returns>
+        public virtual String Chaine()
+        {
+            return "";
+        }
+    }
+}
---
+++ UnitTestProjectVoitureOnLine/UnitTestPrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using classLibraryVoitureOnLine;
+
+namespace UnitTestProjectVoitureOnLine
+{
+    [TestClass]
+    public class UnitTestPrix
+    {
+        [TestMethod]
+        public void TestPrixVoiture()
+        {
+            /*
+            Citadine uneC = new Citadine("Young", "Urban", "Métallisée", "Hybride", false);
+            Citadine uneC2 = new Citadine("Young", "Urban", "Métallisée", "Essence", true);
+            Citadine uneC3 = new Citadine("Young", "Urban", "Métallisée", "Gasoil", false);
+            Compacte uneCo1 = new Compacte("Young", "Urban", "Métallisée", "Essence", true);
+            Compacte uneCo2 = new Compacte("Young", "Urban", "Métallisée", "Essence", false);
+            Familiale uneF = new Familiale("Young", "Urban", "Métallisée", "Gasoil");
+            Limousine uneL = new Limousine("Young", "Urban", "Métallisée", "Hybride");
+            Assert.AreEqual(13000, uneC.Prix());
+            Assert.AreEqual(10000, uneC2.Prix());
+            Assert.AreEqual(10000, uneC3.Prix());
+            Assert.AreEqual(20000, uneCo1.Prix());
+            Assert.AreEqual(17000, uneCo2.Prix());
+            Assert.AreEqual(25000, uneF.Prix());
+            Assert.AreEqual(35000, uneL.Prix());
+            */
+        }
+    }
+}
